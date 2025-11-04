882fbc5e7df92d38fa1874c85c0ea40c1db203d7
L3VI04 <ljenkins@anamosa.k12.ia.us>
2025-11-04 13:36:13 -0600

    Define Person class and instantiate guests
    Added a Person class and created a list of instances.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
+// Define the Person class
+class Person {
+  constructor(firstName, lastName) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+}
+
+// Create a list of three Person instances
+const guests = [
+  new Person("Alice", "Johnson"),
+  new Person("Bob", "Smith"),
+  new Person("Charlie", "Brown")
+];
+
 
